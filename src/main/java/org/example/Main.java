fb5b40fa4ef3d33af512c51dcf45db6bd16e4b58
kazekage__ <sabanovdaniil30@gmail.com>
2023-11-10 23:09:56 +0300

    тест библиотеки

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,74 +1,28 @@
 package org.example;
 
-import java.io.*;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Получение путей до файлов
+        System.out.println("Введите путь до файла , который хотите сжать :");
+        String input_file_path = readFromConsole();
+        System.out.println("Введите путь до файла , в который хотите записать сжатый файл :");
+        String archive_file_path = readFromConsole();
+        System.out.println("Введите путь до файла , в который хотите расжать файл :");
+        String output_file_path = readFromConsole();
 
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите путь к входному файлу: ");
-        String input = scanner.nextLine();
-
-
-        System.out.print("Введите путь к файлу в который хотите записать сжатый файл : ");
-        String archive = scanner.nextLine();
-
-
-        System.out.print("Введите путь к выходному файлу: ");
-        String output = scanner.nextLine();
-
-
+        //Логика
         LZ77 lz77 = new LZ77();
+        lz77.compress(input_file_path,archive_file_path);
+        lz77.decompress(output_file_path,archive_file_path);
 
 
-        // Чтение данных из input.txt
-        StringBuilder inputData = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(input));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                inputData.append(line);
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        // Сжатие данных
-        ArrayList<LZ77.Trio> compressedData = lz77.compress(inputData.toString() , 500);
-        for (int i = 0; i < compressedData.size();i++){
-            System.out.println("<"+compressedData.get(i).length+","+compressedData.get(i).offset+","+compressedData.get(i).nextChar+">");
-        }
-        // Преобразование сжатых данных в байты и запись в архивный файл
-        try {
-            byte[] compressedBytes = lz77.convertToBytes(compressedData);
-            FileOutputStream fileOutputStream = new FileOutputStream(archive);
-            fileOutputStream.write(compressedBytes);
-            fileOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    }
 
-        // Чтение сжатых данных из архивного файла
-        try {
-            FileInputStream fileInputStream = new FileInputStream(archive);
-            byte[] readBytes = new byte[fileInputStream.available()];
-            fileInputStream.read(readBytes);
-            fileInputStream.close();
-
-            // Обратное преобразование из байтов и распаковка данных
-            ArrayList<LZ77.Trio> readCompressedData = lz77.convertFromBytes(readBytes);
-            String decompressedData = lz77.decompress(readCompressedData);
-
-            // Запись распакованных данных в output.txt
-            BufferedWriter writer = new BufferedWriter(new FileWriter(output));
-            writer.write(decompressedData);
-            writer.close();
-        } catch (Exception e){
-            System.out.println("erorr");
-        }
+    public static String readFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
